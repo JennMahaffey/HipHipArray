74e7b077675885748ea0df10a053aebf291e1a24
jennifer mahaffey <jmahaffey1.prsvr@gmail.com>
2023-10-16 08:05:56 -0400

    commit

--- main.js
+++ main.js
@@ -24,27 +24,32 @@ const students = [
 ];
 
 for(i = 0; i < students.length; i++){
-  console.log(students[i]);
+  //console.log(students[i]);
 }
 
-// Challenge 2
-// Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
-const grades = [100, 80, 110, 75, 83, 64];
-for(let i = grades.length - 1; i >= 0; i--){
-  console.log(grades[i]);
+// // Challenge 2
+// // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
+// const grades = [100, 80, 110, 75, 83, 64];
+// for(let i = grades.length - 1; i >= 0; i--){
+//   //console.log(grades[i]);
 
 // Challenge 3
-// Console.log out only the even numbers in the following array.
-const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
-for(let i = 0; i < positiveNumbers.length; i++){
-  if(positiveNumbers[i])% 2 === 0)
-  console.log(positiveNumbers[i]);
-}
+// // Console.log out only the even numbers in the following array.
+// const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
+// for(let i = 0; i < positiveNumbers.length; i++){
+//   if(positiveNumbers[i] % 2 === 0){
+// console.log(positiveNumbers[i]);
+// } 
+// }
 
 // Challenge 4
 // Console.log out the even numbers in the following array, INCLUDING the negative ones. There should be four total printings.
 const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
-
+for(let i = 0; i < mixedSignNumbers.length; i++){
+  if(mixedSignNumbers[i] % 2 === 0){
+console.log(mixedSignNumbers[i]);
+}
+}
 
 // Challenge 5
 // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
